e820821e9d54a2ab0204ab1a00e8bb2a06138f60
guneysus <serefguneysu@gmail.com>
2014-01-01 22:01:30 +0200

    Arşiv tamam gibi Typo fix 1989
    Signed-off-by: guneysus <serefguneysu@gmail.com>

--- markdown/libsplit.py
+++ markdown/libsplit.py
@@ -16,18 +16,26 @@ aylar = [u'Ocak',
          u'Kasım',
          u'Aralık']
 
-f = open('_log.log', 'wb', 'utf-8')
 
-def page2filename(page):
+def page2filename(page, year):
     """docstring for page2filename"""
     page = page.strip('\n').splitlines()
     body = u''''''
     date = page[:1][0]
     title = page[1:2][0]
-    #for row in page[1:]:
-    #    body += row
+
+    for row in page[1:]:
+        body += row + '\n'
 
     day = date.split(' ')[0]
     month = date.split(' ')[1].strip(',')
     monthindex = aylar.index(month) + 1
-    return (date, title, month, day, monthindex )
+    dump2file(year, monthindex, day, body)
+    return (date, title, month, day, monthindex)
+
+
+def dump2file(year, month, day, data):
+    """ docstring for dump2file """
+    f = open('%s/%s-%s.markdown' %(year, month,day), 'wb', 'utf-8')
+    f.write( data )
+    return None
--- markdown/split.py
+++ markdown/split.py
@@ -10,17 +10,19 @@ def main():
     "docstring for split"""
     from codecs import open
     yillar = [ y for y in range(1989,1998) if y != 1993 ]
-    for i in yillar:
+    #for yil in [1989]:
+    for yil in yillar:
         total = 0
-        f = open('%s.markdown' %i, 'rb', 'utf-8')
+        f = open('%s.markdown' %yil, 'rb', 'utf-8')
         year = f.read().split('---')
         year.pop(0)
         for page in year:
-            date, title, month, day, mind = page2filename(page)
+        #for page in year[:1]:
+            date, title, month, day, mind = page2filename(page, yil)
             #if len(title) == 0 or len(month) == 0 or len(day) == 0:
             data = u'%s |  %s %s \t | %s' \
-                    %(i,day, month, title )
+                    %(yil, day, month, title)
             logs.write( data + '\n')
-            print data
+            #print data
 if __name__ == '__main__':
     main()
